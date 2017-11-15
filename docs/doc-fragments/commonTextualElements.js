c4a5da944f13c6e0da03c787b7413c967da6c8fd
Angelos Chalaris <chalarangelo@gmail.com>
2017-11-15 13:19:00 +0200

    Core documentation
    Documented all the elements in the core module. Everything works as expected so far.

--- docs/doc-fragments/commonTextualElements.js
+++ docs/doc-fragments/commonTextualElements.js
@@ -5,7 +5,7 @@ module.exports = {
   keywords: ['p', 'paragraph', 'text', 'textual elements', 'strong', 'bold', 'b', 'em', 'i', 'emphasis', 'italics', 'small', 'a', 'link', 'hr', 'horizontal rule', 'sub', 'subscript', 'sup', 'exponent', 'superscript', 'normalize', 'reset'],
   description: `<p><strong>mini.css</strong> utilizes the ruleset of <a href="http://necolas.github.io/normalize.css/" targe="_blank">Normalize.css</a> v7.0.0 to reliably deal with inconsistencies between browsers, ensuring that your web apps will look beatiful no matter on what browser or device you're at. On top of these rules, we have applied some tasteful rules, such as using <a href="https://www.smashingmagazine.com/2015/11/using-system-ui-fonts-practical-guide/" target="_blank">native font stack</a> to figure out the best font for each device, slightly altering the background and foreground colors, as well as setting the size of the text to <code>16px</code> and its line height to <code>1.5</code>.</p>
   <p>All of the most common HTML5 elements, such as paragraphs, links, bold, small and slanted text, have been styled by default using clean, modern typography to make your pages look cool and stand out from the rest of the internet.</p>`,
-  example: `<p>This is a paragraph with some sample text. Did you know that the latest version of <strong>mini.css</strong> is codenamed <em>Gluon</em>? Well, now you do!</p><hr/><p><small>Remember that <strong>mini.css</strong> is totally free, no fine print involved!</small></p>`,
+  example: `<p class="doc">This is a paragraph with some sample text. Did you know that the latest version of <strong class="doc">mini.css</strong> is codenamed <em class="doc">Gluon</em>? Well, now you do!</p><hr class="doc"/><p><small class="doc">Remember that <strong class="doc">mini.css</strong> is totally free, no fine print involved!</small></p>`,
   samples: [
 `<pre><span class="code-line"><span class="highlight-a">&lt;p&gt;</span>This is a paragraph. with some <span class="highlight-a">&lt;strong&gt;</span>bold text<span class="highlight-a">&lt;/strong&gt;</span> and some <span class="highlight-a">&lt;em&gt;</span>italics text<span class="highlight-a">&lt;/em&gt;</span>.<span class="highlight-a">&lt;/p&gt;</span></span>
 <span class="code-line"><span class="highlight-a">&lt;a</span> <span class="highlight-b">href</span>=<span class="highlight-c">&quot;#&quot;</span><span class="highlight-a">&gt;</span>This is a link.<span class="highlight-a">&lt;/a&gt;</span></span>
@@ -20,9 +20,10 @@ module.exports = {
   customization: [
     `Text color can be changed globally by changing the value of the <code>--fore-color</code> variable.`,
     `Background color can be changed globally by changing the value of the <code>--back-color</code> variable.`,
+    `Border color can be changed globally by changing the value of the <code>--border-color</code> variable. This affects the color of <code>&lt;hr&gt;</code> elements.`,
     `Universal margin for elements can be changed globally by changing the value of the <code>--universal-margin</code> variable. As a rule of thumb, consider the universal margin to be the distance you want your paragraphs to have from the sides of the screen.`,
     `Universal padding for elements can be changed globally by changing the value of the <code>--universal-padding</code> variable.`,
-    `You can change the color of linkss by changing the values of the <code>--a-link-color</code> and <code>--a-visited-color</code> variables.`
+    `You can change the color of links by changing the values of the <code>--a-link-color</code> and <code>--a-visited-color</code> variables.`
   ],
   modifiers: [],
   dos: [],
---
+++ docs/doc-fragments/images.js
@@ -0,0 +1,20 @@
+module.exports = {
+  id: 'images-captions',
+  title: 'Images &amp; captions',
+  keywords: [`img`, `image`, `responsive`, `responsiveness`, `caption`, `figure`, `figcaption`],
+  description: `<p>Image elements are responsive by default, without the need for any special classes or anything similar. They automatically scale down as necessary to display properly on smaller devices, while retaining their original aspect ratio, while they will never scale above their original size.</p>
+  <p>If you want to add captions to images, you can use HTML5 figure elements, along with their related captions.</p>`,
+  example: '<figure><img src="https://placehold.it/800x600" alt="placeholder"><figcaption class="doc">Image caption</figcaption></figure>',
+  samples: [`<pre><span class="code-line"><span class="highlight-a">&lt;img</span> <span class="highlight-b">src</span>=<span class="highlight-c">&quot;image.png&quot;</span> <span class="highlight-b">alt</span>=<span class="highlight-c">&quot;Image description&quot;</span><span class="highlight-a">/&gt;</span></span></pre>`,
+`<pre><span class="code-line"><span class="highlight-a">&lt;figure&gt;</span></span>
+<span class="code-line">  <span class="highlight-a">&lt;img</span> <span class="highlight-b">src</span>=<span class="highlight-c">&quot;image.png&quot;</span> <span class="highlight-b">alt</span>=<span class="highlight-c">&quot;Image description&quot;</span><span class="highlight-a">/&gt;</span></span>
+<span class="code-line">  <span class="highlight-a">&lt;figcaption&gt;</span>Image caption<span class="highlight-a">&lt;/figcaption&gt;</span></span>
+<span class="code-line"><span class="highlight-a">&lt;/figure&gt;</span></span></pre>`],
+  notes: [`While not mandatory, it is considered a good practice to always add a <code>alt</code> attribute to image elements on your web apps.`],
+  customization: [
+    `You can change the text color of <code>&lt;figcaption&gt;</code> elements by changing the value of the <code>--secondary-fore-color</code> variable.`
+  ],
+  modifiers: [],
+  dos: [],
+  donts: []
+}

7dd5b4fd7bff6609e1b339a081255a32f45c9b67
Angelos Chalaris <chalarangelo@gmail.com>
2017-11-19 14:12:30 +0200

    Updated documentation
    Almost ready!

--- docs/build-docs.js
+++ docs/build-docs.js
@@ -21,20 +21,28 @@ var documentStart = `<!DOCTYPE html><html lang="en"><head>
 </head><body>`;
 var documentEnd = `</body></html>`;
 
-var appShellStart = `<div id="root"><header>
-  <span href="#" class="logo">mini.css</span>
-  <a class="button" href="https://github.com/Chalarangelo/mini.css">Github</a>
-  <label for="doc-drawer-checkbox" class="button drawer-toggle"></label>
+var appShellStart = `<div id="root"><header class="row">
+  <span href="#" class="logo col-sm-3 col-md">mini.css</span>
+  <a class="button col-sm col-md" href="https://github.com/Chalarangelo/mini.css" target="_blank">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github" style="height: 20px; vertical-align: text-top;"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+  <span>&nbsp;Github</span></a>
+  <a class="button col-sm col-md" href="#">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit" style="height: 20px; vertical-align: text-top;"><path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path><polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon></svg>
+  <span>&nbsp;Flavors</span></a>
+  <a class="button col-sm col-md" href="#" target="_blank">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book" style="height: 20px; vertical-align: text-top;"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg>
+  <span>&nbsp;Blog</span></a>
+  <label for="doc-drawer-checkbox" class="button drawer-toggle col-sm"></label>
 </header>
 <div class="row" id="doc-wrapper">`;
 var appShellEnd = `</div></div>`;
 
 var appSidebarStart = `<input id="doc-drawer-checkbox" class="drawer" value="on" type="checkbox">
-<nav class="col-md-4 col-lg-3" id="nav-drawer"><div><input style="width: 100%; margin: 0px;" placeholder="Search..." type="search" id="search-bar" oninput="search()"></div>`;
+<nav class="col-md-4 col-lg-3" id="nav-drawer"><h3>Menu</h3><label for="doc-drawer-checkbox" class="button drawer-close"></label><div><input style="width: 100%; margin: 0px;" placeholder="Search..." type="search" id="search-bar" oninput="search()"></div>`;
 var appSidebarEnd = `<span id="no-results">No results found</span></nav>`;
 
 var mainStart = `<main class="col-sm-12 col-md-8 col-lg-9" id="doc-content">`;
-var mainEnd = `</main>`;
+var mainEnd = `<footer><p><strong>mini.css</strong> was designed and built by <a href="https://github.com/Chalarangelo" target="_blank">@Chalarangelo</a>. Source code licensed under the <a href="https://github.com/Chalarangelo/mini.css/blob/master/LICENSE">MIT License</a>.</p><p>Icons provided by <a href="https://feathericons.com/" target="_blank">Feather</a>. Search powered by <a href="http://fusejs.io/" target="_blank">Fuse.js</a>.</p></footer></main>`;
 
 var documentationFragments = docFragments.map(f => buildFragment(f)).join('<br/>');
 var documentationLinks = docFragments.map(f => buildLink(f)).join('');
@@ -61,7 +69,7 @@ var documentationSearch = `<script>
     }
   }
 </script>
-<style id="search-style"></style>`;
+<style id="search-style">#no-results{display:none;}</style>`;
 
 fs.writeFile(outputPath,
   `${documentStart}${appShellStart}

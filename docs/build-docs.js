e5410756a85193e84e2bb97eda58c725cddb0fb8
Angelos Chalaris <chalarangelo@gmail.com>
2018-05-01 14:28:43 +0300

    Inputs for the core module flavor controls

--- docs/build-docs.js
+++ docs/build-docs.js
@@ -216,7 +216,7 @@ var customizationAppShellStart = `<div id="root"><header class="row">
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="height: 20px; vertical-align: text-top;"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
   <span>&nbsp;Github</span></a>
   <a class="button col-sm col-md" href="#">
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="height: 20px; vertical-align: text-top;"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="height: 20px; vertical-align: text-top;"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
   <span>&nbsp;Docs</span></a>
   <label for="doc-drawer-checkbox" class="button drawer-toggle col-sm"></label>
 </header>
@@ -235,8 +235,7 @@ var customizationLinks = [buildLink(custFragments[0]), '<hr style="padding:0;"/>
 
 function buildCustomizationFragment(fragment){
   var fragmentHtml = `<div id="${fragment.id}" class="card fluid">
-  <h2 class="section double-padded">${fragment.title}</h2>
-  <div class="section">${fragment.content}</div>
+  <h2 class="section double-padded">${fragment.title}</h2>${fragment.content}
 </div>`;
   return fragmentHtml;
 }
